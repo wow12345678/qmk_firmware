5072e7568de4815b812f15d6041cad767b3c0ad2
Finn Weidner <finnweidner76@gmail.com>
2024-07-26 11:20:26 +0200

    tweaks to layout

--- keyboards/mokey/ginkgo65hot/keymaps/wow12345678/keymap.c
+++ keyboards/mokey/ginkgo65hot/keymaps/wow12345678/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT(
         KC_ESC,  DE_1,    DE_2,    DE_3,    DE_4,    DE_5,    DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    DE_MINS, DE_EQL,  KC_BSPC, KC_INS,
         KC_TAB,  DE_Q,    DE_P,    DE_R,    DE_O,    DE_C,    DE_B,    DE_T,    DE_D,    DE_W,    DE_J,    DE_ODIA, DE_HASH, KC_BSLS, KC_DEL,
-        KC_CAPS, DE_Z,    DE_L,    DE_A,    DE_N,    DE_I,    DE_U,    DE_E,    DE_H,    DE_S,    DE_F,    DE_Y,             KC_ENT,  KC_PGUP,
+        MT(MOD_LCTL,KC_ESC), DE_Z,    DE_L,    DE_A,    DE_N,    DE_I,    DE_U,    DE_E,    DE_H,    DE_S,    DE_F,    DE_Y,             KC_ENT,  KC_PGUP,
         KC_LSFT,          DE_X,    DE_V,    DE_G,    DE_M,    DE_K,    DE_ADIA, DE_UDIA, DE_SS,   DE_COMM, DE_DOT,  KC_RSFT, KC_UP,   KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,                                      KC_RALT, MO(1),            KC_LEFT, KC_DOWN, KC_RGHT
     ),
